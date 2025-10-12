43b74d38c551bab138d020c8bdec24e819426c9c
Carlos Ruan <c.ruan@edu.pucrs.br>
2025-10-11 22:53:24 -0300

    projeto criado

---
+++ src/main/java/com/carlosruanpucrs/tc2_microsservico_discovery_server/Tc2MicrosservicoDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.carlosruanpucrs.tc2_microsservico_discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tc2MicrosservicoDiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tc2MicrosservicoDiscoveryServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carlosruanpucrs/tc2_microsservico_discovery_server/Tc2MicrosservicoDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosruanpucrs.tc2_microsservico_discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tc2MicrosservicoDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
