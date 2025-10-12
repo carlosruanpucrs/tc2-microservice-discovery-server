9764b0aa813b8d7d94815a82b01af98b3ec4c0b6
Carlos Ruan <c.ruan@edu.pucrs.br>
2025-10-11 22:57:28 -0300

    feat/config-eureka-server: configurado eureka server

--- src/main/java/com/carlosruanpucrs/tc2_microsservico_discovery_server/Tc2MicrosservicoDiscoveryServerApplication.java
+++ src/main/java/com/carlosruanpucrs/tc2_microsservico_discovery_server/Tc2MicrosservicoDiscoveryServerApplication.java
@@ -2,7 +2,9 @@ package com.carlosruanpucrs.tc2_microsservico_discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class Tc2MicrosservicoDiscoveryServerApplication {
 
